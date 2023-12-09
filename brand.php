911b693bd97faa6a0b63d6ab3154cdc8ec6023b6
tunahan24 <anhsilver2404@gmail.com>
2023-12-09 14:21:09 +0700

    Add brand.php file

---
+++ brand.php
@@ -0,0 +1,51 @@
+<div class="content-brand">
+    <div class="container-brand">
+        <h2 class="content-brand-title">Thương hiệu nổi bật</h2>
+        <div class="content-brand-list">
+            <div class="content-brand-item">
+                <a href="#" class="content-brand-link">
+                    <div class="brand-link-img">
+                        <img src="./assets/img/brand/brand1.png" alt="brand1">
+                    </div>
+                    <div class="brand-title">
+                        <h3 class="brand-name">FOODPOUND</h3>
+                        <p class="brand-des">New Snacks Release</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-brand-item">
+                <a href="#" class="content-brand-link">
+                    <div class="brand-link-img">
+                        <img src="./assets/img/brand/brand2.png" alt="brand1">
+                    </div>
+                    <div class="brand-title">
+                        <h3 class="brand-name">TocoToco</h3>
+                        <p class="brand-des">Happy Tea 100% Organic</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-brand-item">
+                <a href="#" class="content-brand-link">
+                    <div class="brand-link-img">
+                        <img src="./assets/img/brand/brand3.png" alt="brand1">
+                    </div>
+                    <div class="brand-title">
+                        <h3 class="brand-name">Soda Brand</h3>
+                        <p class="brand-des">Soda Tươi Mát. 100% Thiên Nhiên</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-brand-item">
+                <a href="#" class="content-brand-link">
+                    <div class="brand-link-img">
+                        <img src="./assets/img/brand/brand4.png" alt="brand1">
+                    </div>
+                    <div class="brand-title">
+                        <h3 class="brand-name">FARMART</h3>
+                        <p class="brand-des">Xúc xích thịt tươi. Mua 2 tặng 1!</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
