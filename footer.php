61acfb7a1b98ed93a3b58d080ecaefb05586cbbd
tunahan24 <anhsilver2404@gmail.com>
2023-12-09 14:20:08 +0700

    Add footer.php file

---
+++ footer.php
@@ -0,0 +1,108 @@
+<footer>
+    <div class="footer-info">
+        <div class="footer-info-list">
+            <div class="footer-info-item">
+                <div class="footer-info-img">
+                    <img src="./assets/img/footer/icon-rocket.png" alt="rocket">
+                </div>
+                <div class="footer-info-content">
+                    <h3 class="footer-info-title">Miễn phí vận chuyển</h3>
+                    <p class="footer-info-desc">Đối với các đơn hàng trên $200</p>
+                </div>
+            </div>
+            <div class="footer-info-item">
+                <div class="footer-info-img">
+                    <img src="./assets/img/footer/icon-reload.png" alt="rocket">
+                </div>
+                <div class="footer-info-content">
+                    <h3 class="footer-info-title">Hoàn trả 1 & 1</h3>
+                    <p class="footer-info-desc">Hủy sau 1 ngày</p>
+                </div>
+            </div>
+            <div class="footer-info-item">
+                <div class="footer-info-img">
+                    <img src="./assets/img/footer/icon-protect.png" alt="rocket">
+                </div>
+                <div class="footer-info-content">
+                    <h3 class="footer-info-title">Bảo mật an toàn 100%</h3>
+                    <p class="footer-info-desc">Đảm bảo thanh toán an toàn</p>
+                </div>
+            </div>
+            <div class="footer-info-item">
+                <div class="footer-info-img">
+                    <img src="./assets/img/footer/icon-support.png" alt="rocket">
+                </div>
+                <div class="footer-info-content">
+                    <h3 class="footer-info-title">Hỗ trợ 24/7</h3>
+                    <p class="footer-info-desc">Hỗ trợ mọi lúc mọi nơi</p>
+                </div>
+            </div>
+            <div class="footer-info-item">
+                <div class="footer-info-img">
+                    <img src="./assets/img/footer/icon-tag.png" alt="rocket">
+                </div>
+                <div class="footer-info-content">
+                    <h3 class="footer-info-title">Ưu đãi hàng ngày</h3>
+                    <p class="footer-info-desc">Giảm giá lên tới 80%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-footer">
+        <div class="container-main-footer">
+            <div class="main-footer-list">
+                <div class="main-footer-item">
+                    <div class="main-footer-slogan">
+                        <img src="./assets/img/header/logo.png" alt="">
+                        <h3 class="main-footer-title" style="padding-top: 0.7rem;">- Tạp hóa thực phẩm trực tuyến</h3>
+                    </div>
+                    <div class="main-footer-contact">
+                        <p class="contact-item"><i class="fa-solid fa-phone"></i>Hotline 24/7: 0987-654-321</p>
+                        <p class="contact-item"><i class="fa-solid fa-location-dot"></i>Tầng 7, Tòa A1, Trường Đại học Công nghiệp Hà Nội</p>
+                        <p class="contact-item"><i class="fa-solid fa-envelope"></i>tunahan@gmail.com</p>
+                    </div>
+                </div>
+                <div class="main-footer-itemmid">
+                    <div class="main-footer-itemsubmid">
+                        <h3 class="main-footer-title">Chính sách & Điều khoản</h3>
+                        <ul class="footer-list-link">
+                            <li><a href="#" class="footer-item-link">Chính sách bảo mật</a></li>
+                            <li><a href="#" class="footer-item-link">Chính sách hoàn tiền</a></li>
+                            <li><a href="#" class="footer-item-link">Chính sách vận chuyển</a></li>
+                            <li><a href="#" class="footer-item-link">Chính sách đổi trả</a></li>
+                        </ul>
+                    </div>
+                    <div class="main-footer-itemsubmid">
+                        <h3 class="main-footer-title">Trung tâm trợ giúp</h3>
+                        <ul class="footer-list-link">
+                            <li><a href="#" class="footer-item-link">Về chúng tôi</a></li>
+                            <li><a href="#" class="footer-item-link">Câu hỏi thường gặp</a></li>
+                            <li><a href="#" class="footer-item-link">Hoàn tiền</a></li>
+                            <li><a href="#" class="footer-item-link">Liên hệ chúng tôi</a></li>
+                        </ul>
+                    </div>
+                    <div class="main-footer-itemsubmid">
+                        <h3 class="main-footer-title">Liên kết hữu ích</h3>
+                        <ul class="footer-list-link">
+                            <li><a href="#" class="footer-item-link"><i class="fa-brands fa-facebook"></i>Facebook</a></li>
+                            <li><a href="#" class="footer-item-link"><i class="fa-brands fa-youtube"></i>Youtube</a></li>
+                            <li><a href="#" class="footer-item-link"><i class="fa-brands fa-tiktok"></i>Tiktok</a></li>
+                            <li><a href="#" class="footer-item-link"><i class="fa-brands fa-twitter"></i>Twitter</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="main-footer-item">
+                    <h3 class="main-footer-title">Liên kết hữu ích</h3>
+                    <p class="main-footer-desc">Đăng ký ngay để nhận thông tin cập nhật về chương trình khuyến mãi và phiếu giảm giá. <br> Đừng lo lắng! Chúng tôi không thư rác</p>
+                    <div class="form-email">
+                        <input type="text" placeholder="Email của bạn...">
+                        <button class="send-email-btn">Đăng ký</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
