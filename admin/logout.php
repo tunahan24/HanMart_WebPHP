062d664eda074aa1c9dcd3ad981f255a9637ece5
tunahan24 <anhsilver2404@gmail.com>
2023-12-09 14:36:04 +0700

    Add logout.php file

---
+++ admin/logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if(isset($_SESSION['admin_email'])){
+    session_destroy();
+    header('location: login_admin_form.php');
+}else{
+    header('location: login_admin_form.php');
+}
+?>
