9c25e8f82f611bc8b678ed2234d9a2ad19e3c871
tunahan24 <anhsilver2404@gmail.com>
2023-12-09 14:26:31 +0700

    Add list_cat.php file

---
+++ category/list_cat.php
@@ -0,0 +1,87 @@
+<div class="content-category">
+    <div class="container-category">
+        <h2 class="content-category-title">Danh mục phổ biến</h2>
+        <div class="content-category-list">
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/fruits.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Rau củ quả</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/banhmi.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Bánh mì</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/HaiSan.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Hải sản</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/Thit.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Thịt tươi</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/Ruou.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Rượu & đồ uống</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/Coffee.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Trà & cà phê</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/Milk.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Sữa tươi</p>
+                    </div>
+                </a>
+            </div>
+            <div class="content-category-item">
+                <a href="#" class="content-category-link">
+                    <div class="link-img">
+                        <img src="./assets/img/category/pet.png" alt="Fruits">
+                    </div>
+                    <div class="link-title">
+                        <p>Thức ăn cho thú</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
