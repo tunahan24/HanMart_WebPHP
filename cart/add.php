2d90531f2273195b110e5f23ec6c7b68eca2859c
tunahan24 <anhsilver2404@gmail.com>
2023-12-10 15:23:45 +0700

    Add add.php file of cart

---
+++ cart/add.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$product_id=$_GET['product_id'];
+
+if(isset($_SESSION['cart'][$product_id])){
+    $_SESSION['cart'][$product_id]=$_SESSION['cart'][$product_id] + 1;
+}else{
+    $_SESSION['cart'][$product_id] = 1;
+}
+$message = "Thêm vào giỏ hàng thành công!";
+echo "<script type='text/javascript'>alert('$message');window.location.href = '../index.php';</script>";
+?>
