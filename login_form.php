6a6daac39fc153e4e0650820a0a2ebc593833838
tunahan24 <anhsilver2404@gmail.com>
2023-12-10 15:20:24 +0700

    Update login_form.php file

--- login_form.php
+++ login_form.php
@@ -37,74 +37,10 @@
 </head>
 <body>
     <div class="main-login">
-        <header class="main-header">
-            <div class="header-top">
-                <div class="header-top-left">
-                    <ul class="top-list">
-                        <li class="top-item"><a href="#" class="top-link">About us</a><span>|</span></li>
-                        <li class="top-item"><a href="#" class="top-link">Wishlist</a><span>|</span></li>
-                        <li class="top-item"><a href="#" class="top-link">Order Tracking</a></li>
-                    </ul>
-                </div>
-                <div class="header-top-right">
-                    <ul class="top-list">
-                        <li class="top-item"><a href="#" class="top-link">Tiếng Việt</a><span>|</span></li>
-                        <li class="top-item"><a href="#" class="top-link">User</a><span>|</span></li>
-                        <li class="top-item"><a href="./admin/login_admin_form.php" class="top-link">Admin</a></li>
-                    </ul>
-                </div>
-            </div>
-    
-            <div class="header-mid">
-                <div class="container-header-mid">
-                    <div class="header-logo"><a href="index.php"><img src="./assets/img/header/logo.png" alt="logo" class="header-logo-img"></a></div>
-                    <div class="header-search">
-                        <input type="text" class="header-search-input" placeholder="I'm shopping for...">
-                        <div class="header-search-button"><i class="ti-search header-search-icon"></i></div>
-                    </div>
-                    <div class="header-icon">
-                        <a href="#" class="header-icon-item"><i class="fa-solid fa-bell fa-shake" style="color: #f7ac18;"></i><div class="icon-noti">0</div></a>
-                        <a href="#" class="header-icon-item"><i class="fa-solid fa-heart fa-beat" style="color: rgb(231, 3, 3);"></i><div class="icon-noti">0</div></a>
-                        <a href="#" class="header-icon-item"><i class="fa-solid fa-cart-shopping"></i><div class="icon-noti">0</div></a>
-                    </div>
-                </div>
-                
-            </div>
-    
-            <div class="header-bottom">
-                <div class="menu-category">
-                    <i class="fa-solid fa-bars menu-category-icon"></i>
-                    <h2 class="menu-category-header">SHOP BY CATEGORY</h2>
-                    <ul class="category-list">
-                        <li class="category-item">
-                            <a href="#" class="category-link"><i class="fa-solid fa-carrot category-icon"></i>Rau củ quả</a>
-                        </li>
-                        <li class="category-item">
-                            <a href="#" class="category-link"><i class="fa-solid fa-burger category-icon"></i>Bánh mì</a>
-                        </li>
-                        <li class="category-item">
-                            <a href="#" class="category-link"><i class="fa-solid fa-shrimp category-icon"></i>Hải sản đông lạnh</a>
-                        </li>
-                        <li class="category-item">
-                            <a href="#" class="category-link"><i class="fa-solid fa-bacon category-icon"></i>Thịt tươi</a>
-                        </li>
-                        <li class="category-item">
-                            <a href="#" class="category-link"><i class="fa-solid fa-mug-hot category-icon"></i>Trà và cà phê</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="nav">
-                    <ul class="nav1">
-                        <li class="nav1-item"><a href="index.php" class="nav1-link"><i class="ti-home nav1-icon"></i>Home</a></li>
-                        <li class="nav1-item"><a href="#" class="nav1-link">Trang<i class="ti-angle-down nav1-icon"></i></li></a>
-                        <li class="nav1-item"><a href="#" class="nav1-link">Cửa hàng<i class="ti-angle-down nav1-icon"></i></a></li>
-                        <li class="nav1-item"><a href="#" class="nav1-link">Blog</a></li>
-                        <li class="nav1-item"><a href="#" class="nav1-link">Câu hỏi</a></li>
-                        <li class="nav1-item"><a href="#" class="nav1-link">Liên hệ</a></li>
-                    </ul>
-                </div>
-            </div>
-        </header>
+        
+        <?php
+        include_once("header.php");
+        ?>
 
         <!-- Main login -->
         <form action="" method="POST">
