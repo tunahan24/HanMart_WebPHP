85a0aebf881955cb5963e4d5479d0bdd09fb5a4b
tunahan24 <anhsilver2404@gmail.com>
2023-12-10 15:21:41 +0700

    Update cart.php file

--- cart.php
+++ cart.php
@@ -28,38 +28,47 @@
 
     <div class="main-cart">
         <h1>Shopping Cart</h1>
-        <table class="table" cellspacing="0">
-            <thead>
-                <tr>
-                    <th class="product-thumbnail"></th>
-                    <th class="product-name">Product</th>
-                    <th class="product-price ">Price</th>
-                    <th class="product-quantity ">Quantity</th>
-                    <th class="product-subtotal ">Total</th>
-                    <th class="product-remove"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="">
-                    <td class="product-thumbnail"><a href="" ><img src="./assets/img/products/banhmi.jpg" alt="Red"></a></td>
-                    <td class="product-name "><a href="">Banh mi</a></td>
-                    <td class="product-price " >
-                        <div class="box-price"><span class="quantity">$258.39 </span></div>
-                    </td>
-                    <td class="product-quantity">
-                        <div class="product-button">
-                            <!-- <span class="ti-minus"></span> -->
-                            <input class="input-text" name="" type="number" value="1" title="Qty" tabindex="0" step="1" min="1" max="18" required="">
-                            <!-- <span class="ti-plus"></span> -->
-                        </div>
-                    </td>
-                    <td class="product-subtotal " >
-                        <div class="box-price"><span class="price-current">$258.39</span></div>
-                    </td>
-                    <td class="product-remove"><a class="" href="#" ><i class="fa-regular fa-trash-can"></i></a></td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="table">
+            <table class="table" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th class="product-thumbnail"></th>
+                        <th class="product-name">Sản phẩm</th>
+                        <th class="product-price ">Đơn giá</th>
+                        <th class="product-quantity ">Số lượng</th>
+                        <th class="product-subtotal ">Tổng giá</th>
+                        <th class="product-remove"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td class="product-thumbnail"><a href="" ><img src="./assets/img/products/banhmi.jpg" alt="Red"></a></td>
+                        <td class="product-name "><a href="">Banh mi</a></td>
+                        <td class="product-price " >
+                            <div class="box-price"><span class="quantity">$258.39 </span></div>
+                        </td>
+                        <td class="product-quantity">
+                            <div class="product-button">
+                                <!-- <span class="ti-minus"></span> -->
+                                <input class="input-text" name="" type="number" value="1" title="Qty" tabindex="0" step="1" min="1" max="18" required="">
+                                <!-- <span class="ti-plus"></span> -->
+                            </div>
+                        </td>
+                        <td class="product-subtotal " >
+                            <div class="box-price"><span class="price-current">$258.39</span></div>
+                        </td>
+                        <td class="product-remove"><a class="" href="#" ><i class="fa-regular fa-trash-can"></i></a></td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="total-price">
+                <h2>Tổng tiền: 1000000 VNĐ</h2>
+            </div>
+            <div class="cart-button">
+                <button class="back-btn"><i class="ti-arrow-left"></i>Continue Shopping</button>
+                <button class="pay-btn">Thanh toán</button>
+            </div>
+        </div>
     </div>
 
     <!-- Footer -->
