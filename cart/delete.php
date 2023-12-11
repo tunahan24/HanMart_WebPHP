db9438fc00278107a9231543ca84b7b9894516b8
tunahan24 <anhsilver2404@gmail.com>
2023-12-11 17:32:54 +0700

    Add delete.php file of cart

---
+++ cart/delete.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+if (isset($_GET["product_id"])) {
+    $product_id = $_GET["product_id"];
+    unset($_SESSION["cart"]["$product_id"]);
+}
+header("location: ../cart.php");
+?>
