3bfc4b7b4bc69f2ca05b223caf7ae6b61a418001
tunahan24 <anhsilver2404@gmail.com>
2023-12-13 23:39:28 +0700

    Update add.php file of cart

--- cart/add.php
+++ cart/add.php
@@ -8,6 +8,5 @@ if(isset($_SESSION['cart'][$product_id])){
     $_SESSION['cart'][$product_id] = 1;
 }
 $message = "Thêm vào giỏ hàng thành công!";
-$redirectURL = isset($_SESSION["user_name"]) ? 'profile.php' : 'index.php';
-echo "<script type='text/javascript'>alert('$message');window.location.href = '../$redirectURL';</script>";
+echo "<script type='text/javascript'>alert('$message');window.location.href = '../index.php';</script>";
 ?>
