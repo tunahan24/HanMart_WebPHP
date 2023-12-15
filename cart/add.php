0f78923fbeae2698d7a3c82cd57c09fa09b2ff8b
tunahan24 <anhsilver2404@gmail.com>
2023-12-15 23:29:26 +0700

    Update add.php file of cart

--- cart/add.php
+++ cart/add.php
@@ -8,5 +8,5 @@ if(isset($_SESSION['cart'][$product_id])){
     $_SESSION['cart'][$product_id] = 1;
 }
 $message = "Thêm vào giỏ hàng thành công!";
-echo "<script type='text/javascript'>alert('$message');window.location.href = '../index.php';</script>";
+echo "<script type='text/javascript'>alert('$message');window.location.href = '../cart.php';</script>";
 ?>
