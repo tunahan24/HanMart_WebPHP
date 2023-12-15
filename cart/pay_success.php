1a92c9ae206587bac9ff1c4c4e211c8292794cea
tunahan24 <anhsilver2404@gmail.com>
2023-12-15 23:29:05 +0700

    Add pay_success.php file

---
+++ cart/pay_success.php
@@ -0,0 +1,11 @@
+<div class="thanks">
+    <p>Quý khách đã đặt hàng thành công!</p>
+    <p>- Sản phẩm của bạn sẽ được chuyển tới địa chỉ trước đó sau thời gian 2-3 ngày tính từ thời điểm này</p>
+    <p>- Nhân viên giao hàng sẽ liên hệ với bạn qua số điện thoại trước khi giao hàng 24h</p>
+    <p>Cảm ơn bạn đã sử dụng sản phẩm của chúng tôi!</p>
+</div>
+<div class="cart-button">
+    <div>
+        <a href="index.php"><button class="back-btn"><i class="ti-arrow-left"></i>Continue Shopping</button></a>
+    </div>
+</div>
