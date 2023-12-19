1dfc4e28a90f8446bc2669fd793b1c8aaa0f725e
tunahan24 <anhsilver2404@gmail.com>
2023-12-19 14:52:55 +0700

    Update add.php file of cart

--- cart/add.php
+++ cart/add.php
@@ -1,20 +1,11 @@
 <?php
 session_start();
 $product_id=$_GET['product_id'];
-$sl=$_POST['sl'];
 
 if(isset($_SESSION['cart'][$product_id])){
-    if(isset($_POST['sl'])){
-        $_SESSION['cart'][$product_id]=$_SESSION['cart'][$product_id] + $sl;
-    }else{
-        $_SESSION['cart'][$product_id]=$_SESSION['cart'][$product_id] + 1;
-    }
+    $_SESSION['cart'][$product_id]=$_SESSION['cart'][$product_id] + 1;
 }else{
-    if(isset($_POST['sl'])){
-        $_SESSION['cart'][$product_id]=$sl;
-    }else{
-        $_SESSION['cart'][$product_id] = 1;
-    }
+    $_SESSION['cart'][$product_id] = 1;
 }
 $message = "Thêm vào giỏ hàng thành công!";
 echo "<script type='text/javascript'>alert('$message');window.location.href = '../cart.php';</script>";
