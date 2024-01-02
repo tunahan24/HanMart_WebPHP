49aafcb283a730a8272520ffd99ac8a257392ecb
tunahan24 <anhsilver2404@gmail.com>
2024-01-02 14:11:24 +0700

    Add slider.php file

---
+++ slider.php
@@ -0,0 +1,25 @@
+<div class="slider">
+    <div class="content-slider">
+        <div class="main-slider">
+            <div class="slider-img">
+                <img src="./assets/img/slider/slider1.png" alt="slider" class="main-slider-img">
+                <img src="./assets/img/slider/slider2.png" alt="slider" class="main-slider-img">
+                <img src="./assets/img/slider/slider3.jpg" alt="slider" class="main-slider-img">
+            </div>
+            <div class="btn-next">
+                <div class="btn-left btn-slider">
+                    <i class="fa-solid fa-chevron-left"></i>
+                </div>
+                <div class="btn-right btn-slider">
+                    <i class="fa-solid fa-chevron-right"></i>
+                </div>
+            </div>
+        </div>
+        <div class="sub-slider">
+            <a href="#" class="sub-slider-link">
+                <img src="./assets/img/slider/subslider.jpg" alt="subslider" class="sub-slider-img">
+            </a>
+        </div>
+    </div>
+</div>
+<script src="./assets/js/slider.js" ></script>
