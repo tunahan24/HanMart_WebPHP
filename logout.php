f7ebf7c46871174b5e020fe099987f1469fa4593
tunahan24 <anhsilver2404@gmail.com>
2024-09-21 21:06:58 +0700

    Update logout.php file

--- logout.php
+++ logout.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-if(isset($_SESSION['user_email'])){
+if(isset($_SESSION['user_name'])){
     session_destroy();
+    unset($_SESSION['access_token']);
     header('location: login_form.php');
 }else{
     header('location: login_form.php');
