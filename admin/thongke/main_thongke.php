a147694d6f730bd956434fdcfac9b3500f8dcbe4
tunahan24 <anhsilver2404@gmail.com>
2024-09-21 21:56:40 +0700

    Add main_thongke.php file

---
+++ admin/thongke/main_thongke.php
@@ -0,0 +1,13 @@
+<div class="container-content-admin">
+    <h1 style="color: aliceblue; margin: 2.4rem">Thống kê doanh thu theo: <span id="text-date"></span></h1>
+    <p style="margin: 2.4rem">
+        <select class="select-date">
+            <option value="7ngay">7 ngày qua</option>
+            <option value="28ngay">28 ngày qua</option>
+            <option value="90ngay">90 ngày qua</option>
+            <option value="365ngay">365 ngày qua</option>
+        </select>
+    </p>
+    <div id="chart" style="height: 450px; margin: 1.8rem; "></div>
+
+</div>
